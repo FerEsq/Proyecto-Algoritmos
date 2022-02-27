77d87e328d093381e7a1a6a251334c3648530d9f
Montoya086 <mon21552@uvg.edu.gt>
2022-02-27 17:59:48 -0600

    Errores

---
+++ src/Lexer.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+
+/**
+ * @author Andres
+ *
+ */
+public class Lexer {
+	/**
+	 * 
+	 * @param expression expression to evaluate
+	 * @return true if the expression is correct
+	 * @throws InterpreterException exception of the interpreter
+	 */
+	public boolean general_evaluation(String expression) throws InterpreterException{
+		boolean result=false;
+		boolean parenthesis=false;
+		//evaluate if parenthesis exist 
+		if(expression.length()!=0) {
+			if(expression.trim().charAt(0)=='('&&expression.trim().endsWith(")")) {
+				result=true;
+			}else {
+				result = false;
+				throw new InterpreterException("Parenthesis missing");
+			}
+			//evaluate if the pair of parenthesis are complete
+			int incomplete_pairs=0;
+			for(int i=0; i<expression.length();i++) {
+				if(expression.charAt(i)=='(') {
+					parenthesis=true;
+					incomplete_pairs++;
+				}else if(expression.charAt(i)==')'){
+					parenthesis=true;
+					incomplete_pairs--;
+				}
+			}
+			if(incomplete_pairs==0&&parenthesis) {//pairs are complete
+				result=true;
+			}else if (incomplete_pairs>0) { //there's more opening parenthesis than closing ones
+				result = false;
+				throw new InterpreterException("Closing Parenthesis missing");
+			}else if (incomplete_pairs<0) {//there's more closing parenthesis than opening ones
+				result = false;
+				throw new InterpreterException("Opening Parenthesis missing");
+			}
+		}
+		return result;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
  * 
  */
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 /**
  * @author Andres
  *
@@ -15,41 +13,26 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		try {
-			System.out.println(evaluate(sc.nextLine()));
-		}catch (InterpreterException e) {
-			System.out.println(e);
-		}
-	}
-	private static boolean evaluate(String expression) throws InterpreterException{
-		boolean result=false;
-		boolean parenthesis=false;
-		int incomplete_pairs=0;
-		for(int i=0; i<expression.length();i++) {
-			if(expression.charAt(i)=='(') {
-				parenthesis=true;
-				incomplete_pairs++;
-			}else if(expression.charAt(i)==')'){
-				parenthesis=true;
-				incomplete_pairs--;
+		Lexer lexer = new Lexer();
+		boolean run = true;
+		System.out.println("WELCOME TO THE LISP INTERPRETER \nTO LEAVE, WRITE \"leave\"");
+		while(run) {
+			String expressionString=sc.nextLine();
+			if(expressionString.trim().toLowerCase().equals("leave")) {//leave
+				run=false;
+				System.out.println("Goodbye!");
+			}else {
+				boolean evaluation=false;
+				try {
+					evaluation = lexer.general_evaluation(expressionString);
+				}catch (InterpreterException e) {
+					System.out.println(e);					}
+					if(evaluation) {
+					//expression is correct
+					System.out.println("Correct");
+				}
 			}
 		}
-		if(incomplete_pairs==0&&parenthesis) {
-			result=true;
-		}else if (incomplete_pairs>0) {
-			result = false;
-			throw new InterpreterException("Closing Parenthesis missing");
-		}else if (incomplete_pairs<0) {
-			result = false;
-			throw new InterpreterException("Opening Parenthesis missing");
-		}
-		
-		if(expression.trim().charAt(0)=='('&&expression.trim().endsWith(")")) {
-			result=true;
-		}else {
-			result = false;
-			throw new InterpreterException("Parenthesis missing");
-		}
-		return result;
 	}
+	
 }
