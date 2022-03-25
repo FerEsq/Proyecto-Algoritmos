9f7dab82f546209787cb02bdeaef2c353bfa4d73
Deloz09 <84214640+Deloz09@users.noreply.github.com>
2022-03-24 22:56:19 -0600

    Test 2
    Correccion tests

--- src/FunctionsTests.java
+++ src/FunctionsTests.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -62,6 +63,45 @@ class FunctionsTests {
 		String[] instructions = {"setq res * res cont", "setq cont + cont 1", "cond (> cont x)(print res)(t(factorial x)))"};
 		fnt.newFunction(name, parameter, instructions);
 		assertEquals("(setq res * res cont)(setq cont + cont 1)(cond (> cont x)(print res)(t(factorial x))))", fnt.functionCall(name, parameter));
+	}
+	
+	@Test
+	void divisionFunctionTest1() throws InterpreterException {
+		Functions fnt = new Functions();
+		
+		String name = "fun";
+		String parameters = "x";
+		
+		Throwable exception = assertThrows(InterpreterException.class, () -> fnt.functionCall(name, parameters));
+	    assertEquals("Function fun not found", exception.getMessage());
+	}
+	
+	@Test
+	void divisionFunctionTest2() throws InterpreterException {
+		Functions fnt = new Functions();
+		
+		String name = "fun";
+		String parameters1 = "x,y";
+		String parameters2 = "x";
+		String[] instructions = {"print / x y"};
+		fnt.newFunction(name, parameters1, instructions);
+		
+		Throwable exception = assertThrows(InterpreterException.class, () -> fnt.functionCall(name, parameters2));
+	    assertEquals("Missing parameters", exception.getMessage());
+	}
+	
+	@Test
+	void divisionFunctionTest3() throws InterpreterException {
+		Functions fnt = new Functions();
+		
+		String name = "fun";
+		String name2 = "fer";
+		String parameters = "x,y";
+		String[] instructions = {"print / x y"};
+		fnt.newFunction(name, parameters, instructions);
+		
+		Throwable exception = assertThrows(InterpreterException.class, () -> fnt.functionCall(name2, parameters));
+	    assertEquals("Function fer not found", exception.getMessage());
 	}
 	
 }
